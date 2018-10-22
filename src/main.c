ef09eee8f9c8f55230883b0e189bcd7265c2c390
kilometers <kilo@meters.sh>
2018-10-21 17:27:09 -0700

    PNG rendering

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 #include <stdint.h>
 #include <string.h>
 #include <time.h>
-// #include <png.h>
+#include <png.h>
 
 static void print_help(void) { 
     fprintf(stderr,
@@ -62,6 +62,71 @@ static bool *game_board_new(
     return game_board;
 }
 
+static void format_board_as_png(
+    const bool *game_board,
+    const int size,
+    const int height,
+    const int width
+) {
+    FILE *fp = fopen("conway.png", "wb");
+    if (fp == NULL) {
+        fprintf(stderr, "error: Could not open \"conway.png\" for writing\n");
+        exit(1);
+    }
+
+    png_structp png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING,
+                                      NULL, NULL, NULL);
+    if (png_ptr == NULL) {
+        fprintf(stderr, "error: Not enough memory\n");
+        exit(1);
+    }
+
+    png_infop info_ptr = png_create_info_struct(png_ptr);
+    if (info_ptr == NULL) {
+        fprintf(stderr, "error: Not enough memory\n");
+    }
+
+    png_init_io(png_ptr, fp);
+
+    // bit depth of 2 since it's a black and white image
+    png_set_IHDR(png_ptr, info_ptr, width * size, height * size, 8,
+                 PNG_COLOR_TYPE_GRAY, PNG_INTERLACE_NONE,
+                 PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
+
+    png_text title_text;
+    title_text.compression = PNG_TEXT_COMPRESSION_NONE;
+    title_text.key = "Title";
+    title_text.text = "title";
+    png_set_text(png_ptr, info_ptr, &title_text, 1);
+
+    png_write_info(png_ptr, info_ptr);
+
+    // 1 byte per pixel 
+    png_bytep row = (png_bytep) malloc(1 * width * size * sizeof(png_byte));
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width * size; j++) {
+            if (j % size == 0) {
+                bool cell = game_board[i * width + j];
+                png_byte pixel_color;
+                if (cell) {
+                    pixel_color = 255;
+                } else {
+                    pixel_color = 0;
+                }
+                for (int s = 1; s <= size; s++) {
+                    row[j + s] = pixel_color;
+                }
+            }
+        }
+        for (int s = 0; s < size; s++) {
+            png_write_row(png_ptr, row);
+        }
+    }
+
+    png_write_end(png_ptr, NULL);
+}
+
 int main(int argc, char *argv[]) {
     if (argc != 5
         || !strcmp(argv[1], "--help")
@@ -80,6 +145,8 @@ int main(int argc, char *argv[]) {
     game_board_new(game_board, height * width);
 
     print_game_board(game_board, height, width);
+
+    format_board_as_png(game_board, size, height, width);
 
     printf("%d %d %d %d\n", n, width, height, size);
     return 0;
